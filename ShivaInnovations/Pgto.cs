573c0ddd56cfb3130de9e8f78e173705e1a60c49
Gomesgaab <gabrielgabiga870@gmail.com>
2023-08-15 17:14:45 -0300

    Commit Final

--- ShivaInnovations/Pgto.cs
+++ ShivaInnovations/Pgto.cs
@@ -1,85 +1,124 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace ShivaInnovations
 {
     public partial class Pgto : Form
     {
         DAO conectar;
-        ControlCPF modelo;
-        bool validar = true;
+        bool validar = true; 
         public Pgto()
         {
-            
             InitializeComponent();
             conectar = new DAO();  //Ligando o Banco de dados a tabela
-            modelo = new ControlCPF();
 
         }//Fim do construtor
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+            bool valid = true;
+            string cpf = maskedTextBox1.Text.Replace(",", "").Replace("-", "");
 
-            // validando preenchimento 
-            if ((maskedTextBox1.MaskCompleted == false) || (validar == false))
+            // Verificar se todos os dígitos são iguais (CPF inválido)
+            if (valid)
+            {
+                for (byte i = 0; i < 10; i++)
+                {
+                    var temp = new string(Convert.ToChar(i), 11);
+                    if (cpf == temp)
+                    {
+                        valid = false;
+                        break;
+                    }// fim do se
+
+                }// fim do para
+            }// fim so se
+
+            // verificar os digitos de controle do cpf "00"
+            if (valid)
+            {
+                var j = 0;
+                var d1 = 0; // guardar o primeiro digito
+                var d2 = 0; // guardar o segundo digito
+
+                // validando o primeiro digito de controle
+                for (int i = 10; i > 1; i--)
+                {
+                    d1 += Convert.ToInt32(cpf.Substring(j, 1)) * i;
+                    j++;
+                }// fim
+
+                // resto da divisão
+                d1 = (d1 * 10) % 11;
+                if (d1 == 10)
+                {
+                    d1 = 0;
+                }// fim
+
+                // verificando se o primeiro número e valido
+                if (d1 != Convert.ToInt32(cpf.Substring(9, 1)))
+                {
+                    valid = false;
+                }// f im
+
+                // validar segundo número
+                j = 0;
+
+                for (int i = 10; i > 1; i--)
+                {
+                    d2 += Convert.ToInt32(cpf.Substring(j, 1)) * i;
+                    j++;
+                }// fim
+
+                // resto da divisão
+                d2 = (d2 * 10) % 11;
+                if (d2 == 10)
+                {
+                    d2 = 0;
+                }// fim
+
+                // verificando se o primeiro número e valido
+                if (d2 != Convert.ToInt32(cpf.Substring(10, 1)))
+                {
+                    valid = false;
+                }// fim
+            }
+            valid = true;
+
+            if ((maskedTextBox1.MaskFull == false) || (valid == false))
             {
 
-                MessageBox.Show("O campo CPF invalido!");
-                MessageBox.Show(Convert.ToString(validar));
+                MessageBox.Show("O campo CPF deve ser preenchido!");
                 maskedTextBox1.BackColor = Color.Red;
             }
-            else if ((maskedTextBox1.MaskCompleted == true) && (validar == true))
+            else if ((maskedTextBox1.MaskFull == true) && (valid == true))
             {
                 try
                 {
-                    
-                    string result = conectar.Inserir(Convert.ToInt64(maskedTextBox1.Text), nomebox.Text, email.Text, telefone.Text, ingressos.Text, "Cliente");
+                    string result = conectar.Inserir(Convert.ToInt64(cpf), nomebox.Text, email.Text, telefone.Text, ingressos.Text, "Cliente");
                     MessageBox.Show(result);
-                    validar = false;
+                    MessageBox.Show(cpf);
                 }
-                catch (Exception error)
+                catch (Exception erro)
                 {
-                    MessageBox.Show("Algo deu errado\n\n" + error);
-                    MessageBox.Show(Convert.ToString(validar));
-                   
-
-
-
-                    validar = true;
+                    MessageBox.Show("Algo deu errado\n\n" + erro.Message);
+                    MessageBox.Show(cpf);
                 }//Fim do TryCatch
             }
+
+
+
         }//Botão comprar
 
- 
+
 
         private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
-            string cpf = maskedTextBox1.Text.Replace(",", "").Replace("-", "");
-            validar = this.modelo.Validarcpf(cpf); //Chamando o método
+            string cpf = maskedTextBox1.Text.Replace(".", "").Replace("-", "");
 
-
-
-            if (validar)
-            {
-                MessageBox.Show("CPF INVÁLIDO");
-            }
-            else
-            {
-                MessageBox.Show("CPF VÁLIDO");
-
-            }//Fim do IF...Else
-
+          
         }// cpf
 
         private void nomebox_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
